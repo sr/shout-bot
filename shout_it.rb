442d60b0eb43314c5703f12c2b8815d96b928503
ichverstehe <ichverstehe@gmail.com>
2008-11-17 18:26:17 +0100

    examplez

--- shout_it.rb
+++ shout_it.rb
@@ -4,10 +4,9 @@ ShoutBot.new('shout_bot', 'irc.freenode.net', 6667) do |bot|
   bot.join "#awesome_channel" do |c|
     c.shout "This is stupid."
     c.shout "And retarded."
-    c.shout "I quit!"
-    c.shout ".. and will be back!"
+    c.shout "But, I stay!"
   end
-  bot.join "#integrity" do |c|
+  bot.join "#not_awesome" do |c|
     c.shout "This is stupid."
     c.shout "And retarded."
     c.shout "I quit!"
